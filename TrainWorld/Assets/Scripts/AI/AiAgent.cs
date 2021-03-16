ee01466f3cbd1ee155f8404dfc7f9451a1843152
CHO MINGI <gee03143@gmail.com>
2021-03-16 22:16:26 +0900

    AiAgent Prototype
    두 역의 경로를 찾아가는 agent 구현
    TODO :
    agent의 움직임을 자연스럽게 만들 방법 필요
    여러 역을 순차적으로 통과하도록 기능 추가하여야 함
    역과 기차를 클릭 시 별도의 ui 출력

---
+++ TrainWorld/Assets/Scripts/AI/AiAgent.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrainWorld.AI
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class AiAgent : MonoBehaviour
+    {
+        public event Action OnDeath;
+
+        [SerializeField]
+        private float speed = 1.0f;
+        [SerializeField]
+        private float rotationSpeed = 10.0f;
+
+        public bool loop;
+
+        private bool move;
+
+        private bool stop;
+
+        List<Vertex> path;
+        int currentIndex;
+        Vertex currentTarget;
+
+        public bool Stop
+        {
+            get { return stop; }
+            set
+            {
+                stop = value;
+                if (stop)
+                {
+                    rb.velocity = Vector3.zero;
+                }
+            }
+        }
+
+        Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            path = new List<Vertex>();
+            currentIndex = 0;
+        }
+
+        public void Initialize(List<Vertex> path, bool isLoop)
+        {
+            this.path = path;
+            currentIndex = 1;
+            currentTarget = path[currentIndex];
+            move = true;
+            stop = false;
+            loop = isLoop;
+        }
+
+        private void Update()
+        {
+            if(move && stop == false)
+            {
+                TimeStepping();
+            }
+        }
+
+        private void TimeStepping()
+        {
+            if(path.Count > currentIndex)
+            {
+                float remainingDistance = MoveAgent();
+                if(remainingDistance < 0.1f)
+                {
+                    // step to next position
+                    currentIndex++;
+                    if(currentIndex >= path.Count)
+                    {
+                        if (loop)
+                        {
+                            currentIndex = 0;
+                        }
+                        else
+                        {
+                            move = false;
+                            return;
+                        }
+                    }
+                    currentTarget = path[currentIndex];
+                }
+            }
+        }
+
+        private float MoveAgent()
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.Position, step);
+            rb.velocity = transform.forward * speed;
+
+            Vector3 lookDirection = currentTarget.Position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), Time.deltaTime * rotationSpeed);
+            return Vector3.Distance(transform.position, currentTarget.Position);
+        }
+
+        private void OnDestroy()
+        {
+            OnDeath?.Invoke();
+        }
+    }
+}
--- TrainWorld/Assets/Scripts/Station/StationPlacementManager.cs
+++ TrainWorld/Assets/Scripts/Station/StationPlacementManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using TrainWorld.Rail;
+using TrainWorld.AI;
 
 namespace TrainWorld.Station
 {
@@ -20,7 +21,12 @@ namespace TrainWorld.Station
         [SerializeField]
         private GameObject stationPrefab;
 
+        [SerializeField]
+        private GameObject trainPrefab;
+
         List<TrainStation> stations;
+
+        RailModel railModelAtCursor;
 
         void Awake()
         {
@@ -29,9 +35,7 @@ namespace TrainWorld.Station
 
         internal void PlaceStation(Vector3Int position)
         {
-            RailModel modelAtPos = railModelManager.GetModelAt(position, placementStartDirection);
-
-            TrainStation newStation = modelAtPos.AddStation(stationPrefab);
+            TrainStation newStation = railModelAtCursor.AddStation(stationPrefab);
 
             //이미 station이 존재하는 위치일 경우 false 반환
             if (newStation != null)
@@ -40,26 +44,28 @@ namespace TrainWorld.Station
 
         internal void MoveCursor(Vector3 position)
         {
-            RailModel direction = railModelManager.GetRailModelViaMousePosition(position);
-
-            Debug.Log(direction.ToString());
+            railModelAtCursor = railModelManager.GetRailModelViaMousePosition(position);
         }
 
         internal void RotateCursor()
         {
             placementStartDirection = DirectionHelper.Next(placementStartDirection);
 
-            if(stations.Count > 2)
+            if(stations.Count >= 2)
             {
                 TrainStation depart = stations[0];
                 TrainStation arrival = stations[1];
                 List<Vertex> path = RailGraphPathfinder.AStarSearch(depart.Position, depart.Direction, arrival.Position, true, railPlacementManager.railGraph);
+                GameObject train = Instantiate(trainPrefab, depart.Position, Quaternion.Euler(DirectionHelper.ToEuler(depart.Direction))) as GameObject;
+                AiAgent ai = train.GetComponent<AiAgent>();
+                ai.Initialize(path, false);
+                
                 if (path.Count > 0)
                 {
                     foreach (Vertex vertex in path)
                     {
-                        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        cube.transform.position = vertex.Position;
+                        //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        //cube.transform.position = vertex.Position;
                     }
                 }
                 else
